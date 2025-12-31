e999e4e3f1d34dd6786524e3332be95fcc345fa1
magunus <shiki@example.com>
2025-12-31 12:59:06 +0900

    init amplify auth app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+
+function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
+
+export default App;
